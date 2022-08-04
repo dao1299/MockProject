7e9feb973492e9b9c0691bff00f9e1a6ba55aeb4
Slick <dao1299@gmail.com>
2022-08-04 17:19:07 +0700

    add ListSongAdapter

--- app/src/main/java/com/example/mockproject/adapter/ListSongAdapter.java
+++ app/src/main/java/com/example/mockproject/adapter/ListSongAdapter.java
@@ -1,2 +1,56 @@
-package com.example.mockproject.adapter;public class ListSongAdapter {
+package com.example.mockproject.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.ListAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mockproject.databinding.LayoutItemListSongBinding;
+import com.example.mockproject.model.SongModel;
+
+public class ListSongAdapter extends ListAdapter<SongModel, ListSongAdapter.ViewHolder> {
+    private OnClickItemListSong onClickItemListSong;
+
+    public ListSongAdapter(@NonNull DiffUtil.ItemCallback<SongModel> diffCallback, OnClickItemListSong onClickItemListSong) {
+        super(diffCallback);
+        this.onClickItemListSong = onClickItemListSong;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutItemListSongBinding.inflate(
+                LayoutInflater.from(parent.getContext()),
+                parent,
+                false
+        ));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        SongModel songModel = getItem(position);
+        if (songModel != null) {
+            holder.binding.setSongModel(songModel);
+            holder.binding.setListener(onClickItemListSong);
+            holder.binding.executePendingBindings();
+        }
+    }
+
+    public interface OnClickItemListSong {
+        void onClickItemSong(SongModel songModel, View view);
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        LayoutItemListSongBinding binding;
+
+        public ViewHolder(LayoutItemListSongBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+
+    }
 }
